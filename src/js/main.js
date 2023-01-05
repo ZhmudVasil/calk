7d7ad4f8aa9658d4efa2231ba5ff8445e93e1c3a
Vasil Zhmud <vasil.zhmud1@gmail.com>
2023-01-05 23:31:21 +0200

    add some function

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,4 @@
 let baterrylevel = document.querySelector('.headerCalk__battery');
-
-// let hour = document.querySelector('.hour');
-// let minute = document.querySelector('.minute');
-// let date = document.querySelector('.date');
-
 navigator.getBattery().then((battery) => {
   updateLevelInfo();
   battery.addEventListener('levelchange', () => {
@@ -14,6 +9,9 @@ navigator.getBattery().then((battery) => {
   }
 });
 
+// let hour = document.querySelector('.hour');
+// let minute = document.querySelector('.minute');
+// let date = document.querySelector('.date');
 function clock() {
   const datetime = new Date();
   document.querySelector('.minute').innerHTML = datetime.getMinutes().toString().padStart(2, '0');
@@ -29,3 +27,73 @@ setInterval(() => {
   document.querySelector('.twoDotet').classList.toggle('is-notTwoDotet');
   clock();
 }, 1000);
+
+const input = document.querySelector('.board__text');
+const resultCalk = document.querySelector('.results');
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', '*', '/'];
+let firstNumber = '';
+let secondNumber = '';
+let sign = '';
+let finish = false;
+
+setTimeout(() => {
+  input.value = '';
+}, 1500);
+
+function clearAll() {
+  firstNumber = '';
+  secondNumber = '';
+  sign = '';
+  finish = false;
+  input.value = 0;
+  resultCalk.value = 0;
+}
+
+document.querySelector('.button__null').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+  let target = event.target;
+  let key = event.target.textContent;
+  if (!target.classList.contains('button')) return;
+  if (target.classList.contains('button__null')) return;
+  if (digit.includes(key)) {
+    if (secondNumber === '' && sign === '') {
+      firstNumber += key;
+      input.value = firstNumber;
+    } else if (firstNumber !== '' && secondNumber !== '' && finish) {
+      secondNumber = key;
+      finish = false;
+      input.value = secondNumber;
+    } else {
+      secondNumber += key;
+      input.value = secondNumber;
+      resultCalk.value = firstNumber;
+    }
+  }
+  if (action.includes(key)) {
+    sign = key;
+    input.value = sign;
+    return;
+  }
+  if (key === '=') {
+    if (secondNumber === '') secondNumber = firstNumber;
+    switch (sign) {
+      case '+':
+        firstNumber = +firstNumber + +secondNumber;
+        break;
+      case '-':
+        firstNumber = firstNumber - secondNumber;
+        break;
+      case '*':
+        firstNumber = firstNumber * secondNumber;
+        break;
+      case '/':
+        firstNumber = firstNumber / secondNumber;
+        break;
+    }
+    finish = true;
+    input.value = firstNumber;
+    resultCalk.value = firstNumber;
+  }
+};
